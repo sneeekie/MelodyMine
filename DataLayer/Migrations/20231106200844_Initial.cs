89ec7d2dccc4f43b2d5e90ae2ecd3e7e103e06b2
@adrian.istrefi.nuppnau <adrian.istrefi.nuppnau@lego.com>
2023-11-06 21:12:30 +0100

    New migration

---
+++ DataLayer/Migrations/20231106200844_Initial.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DataLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    AddressId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Postal = table.Column<int>(type: "integer", nullable: false),
+                    StreetNumber = table.Column<int>(type: "integer", nullable: false),
+                    City = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Country = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Street = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.AddressId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    GenreId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    GenreName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.GenreId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vinyls",
+                columns: table => new
+                {
+                    VinylId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Artist = table.Column<string>(type: "text", nullable: true),
+                    Price = table.Column<double>(type: "double precision", nullable: false),
+                    ImagePath = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vinyls", x => x.VinylId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Email = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    BuyDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    AddressId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                    table.ForeignKey(
+                        name: "FK_Orders_Addresses_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Addresses",
+                        principalColumn: "AddressId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VinylGenres",
+                columns: table => new
+                {
+                    VinylId = table.Column<int>(type: "integer", nullable: false),
+                    GenreId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VinylGenres", x => new { x.VinylId, x.GenreId });
+                    table.ForeignKey(
+                        name: "FK_VinylGenres_Genres_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genres",
+                        principalColumn: "GenreId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_VinylGenres_Vinyls_VinylId",
+                        column: x => x.VinylId,
+                        principalTable: "Vinyls",
+                        principalColumn: "VinylId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderProductDetails",
+                columns: table => new
+                {
+                    OrderProductDetailsId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    VinylId = table.Column<int>(type: "integer", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<double>(type: "double precision", nullable: false),
+                    OrderId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderProductDetails", x => x.OrderProductDetailsId);
+                    table.ForeignKey(
+                        name: "FK_OrderProductDetails_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderProductDetails_Vinyls_VinylId",
+                        column: x => x.VinylId,
+                        principalTable: "Vinyls",
+                        principalColumn: "VinylId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Addresses",
+                columns: new[] { "AddressId", "City", "Country", "Postal", "Street", "StreetNumber" },
+                values: new object[,]
+                {
+                    { 1, "Copenhagen", "Denmark", 2400, "Birkedommervej", 29 },
+                    { 2, "Fredericia", "Denmark", 7000, "Dronningsgade", 8 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "GenreId", "GenreName" },
+                values: new object[,]
+                {
+                    { 1, "Alternativ" },
+                    { 2, "HipHop" },
+                    { 3, "Pop" },
+                    { 4, "Christmas" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Vinyls",
+                columns: new[] { "VinylId", "Artist", "ImagePath", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Ukendt Kunstner", "https://moby-disc.dk/media/catalog/product/cache/e7dc67195437dd6c7bf40d88e25a85ce/i/m/image001_9__2.jpg", 127.0, "Dansktop" },
+                    { 2, "Kanye West", "https://moby-disc.dk/media/catalog/product/cache/e7dc67195437dd6c7bf40d88e25a85ce/k/a/kanye-west-2018-ye-compact-disc.jpg", 187.0, "Ye" },
+                    { 3, "Radioheaad", "https://moby-disc.dk/media/catalog/product/cache/e7dc67195437dd6c7bf40d88e25a85ce/b/f/bfea3555ad38fe476532c5b54f218c09_1.jpg", 227.0, "OK Computer" },
+                    { 4, "Frank Ocean", "https://best-fit.transforms.svdcdn.com/production/albums/frank-ocean-blond-compressed-0933daea-f052-40e5-85a4-35e07dac73df.jpg?w=469&h=469&q=100&auto=format&fit=crop&dm=1643652677&s=6ef41cb2628eb28d736e27b42635b66e", 777.0, "Blonde" },
+                    { 5, "Dean Martin", "https://moby-disc.dk/media/catalog/product/cache/e7dc67195437dd6c7bf40d88e25a85ce/m/o/moby-disc-13-09-2023_10.54.44.png", 127.0, "Winter Wonderland" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Orders",
+                columns: new[] { "OrderId", "AddressId", "BuyDate", "Email" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2023, 11, 6, 20, 8, 44, 666, DateTimeKind.Utc).AddTicks(5820), "john@example.com" },
+                    { 2, 2, new DateTime(2023, 11, 6, 20, 8, 44, 666, DateTimeKind.Utc).AddTicks(5820), "adrian@example.com" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "VinylGenres",
+                columns: new[] { "GenreId", "VinylId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 2, 2 },
+                    { 2, 3 },
+                    { 3, 4 },
+                    { 4, 5 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "OrderProductDetails",
+                columns: new[] { "OrderProductDetailsId", "OrderId", "Price", "Title", "VinylId" },
+                values: new object[,]
+                {
+                    { 1, 1, 127.0, "Dansktop", 1 },
+                    { 2, 2, 227.0, "OK Computer", 2 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderProductDetails_OrderId",
+                table: "OrderProductDetails",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderProductDetails_VinylId",
+                table: "OrderProductDetails",
+                column: "VinylId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_AddressId",
+                table: "Orders",
+                column: "AddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VinylGenres_GenreId",
+                table: "VinylGenres",
+                column: "GenreId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderProductDetails");
+
+            migrationBuilder.DropTable(
+                name: "VinylGenres");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+
+            migrationBuilder.DropTable(
+                name: "Vinyls");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+        }
+    }
+}
