c25c4d64840dc9429ed4bfc18d3c1f3a6cd2b1ce
@adrian.istrefi.nuppnau <adrian.istrefi.nuppnau@lego.com>
2023-11-09 15:18:44 +0100

    Removed old tests

--- xUnitTest/OrderTests.cs
+++ xUnitTest/OrderTests.cs
@@ -31,8 +31,8 @@ namespace xUnitTest
                 BuyDate = DateTime.Now,
                 OrderProductDetails = new List<OrderProductDetails>
                 {
-                    new OrderProductDetails { ProductId = 1, Name = "SeededProduct1", Price = 25.0 },
-                    new OrderProductDetails { ProductId = 2, Name = "SeededProduct2", Price = 35.0 }
+                    new OrderProductDetails { OrderProductDetailsId = 1, OrderId = 1, VinylId = 1, Title = "Dansktop", Price = 127 },
+                    new OrderProductDetails { OrderProductDetailsId = 2, OrderId = 2, VinylId = 2, Title = "Ye", Price = 187 },
                 }
             };
         
@@ -63,8 +63,8 @@ namespace xUnitTest
                 BuyDate = DateTime.Now,
                 OrderProductDetails = new List<OrderProductDetails>
                 {
-                    new OrderProductDetails { ProductId = 3, Name = "TestProduct1", Price = 10.0 },
-                    new OrderProductDetails { ProductId = 4, Name = "TestProduct2", Price = 15.0 }
+                    new OrderProductDetails { OrderProductDetailsId = 3, OrderId = 3, VinylId = 3, Title = "OK Computer", Price = 227 },
+                    new OrderProductDetails { OrderProductDetailsId = 4, OrderId = 4, VinylId = 4, Title = "Blonde", Price = 777 },
                 }
             };
 
@@ -76,28 +76,6 @@ namespace xUnitTest
             var createdOrder = context.Orders.Include(o => o.OrderProductDetails).Last();
             Assert.Equal("test@email.com", createdOrder.Email);
             Assert.Equal(2, createdOrder.OrderProductDetails.Count);
-        }
-
-        [Fact]
-        public void CreateOrderProductDetails_AddsMultipleOrderProductDetails()
-        {
-            using var context = new ApplicationDbContext(_dbContextOptions);
-            
-            // Arrange
-            var service = new OrderService(context);
-            int initialCount = context.OrderProductDetails.Count();
-        
-            var newOrderProductDetailsList = new List<OrderProductDetails>
-            {
-                new OrderProductDetails { ProductId = 1, Name = "NewProduct1", Price = 25.0, OrderId = 1 },
-                new OrderProductDetails { ProductId = 2, Name = "NewProduct2", Price = 35.0, OrderId = 1 }
-            };
-
-            // Act
-            service.CreateOrderProductDetails(newOrderProductDetailsList);
-
-            // Assert
-            Assert.Equal(initialCount + 2, context.OrderProductDetails.Count());
         }
 
         [Fact]
@@ -186,25 +164,6 @@ namespace xUnitTest
         }
 
         [Fact]
-        public void GetSingleFullOrderBy_ReturnsOrderWithDetails_WhenIdExists()
-        {
-            using var context = new ApplicationDbContext(_dbContextOptions);
-            
-            // Arrange
-            var service = new OrderService(context);
-            int existingOrderId = 1;
-
-            // Act
-            var order = service.GetSingleFullOrderBy(existingOrderId);
-
-            // Assert
-            Assert.NotNull(order);
-            Assert.Equal(existingOrderId, order.OrderId);
-            Assert.NotNull(order.OrderProductDetails);
-            Assert.NotEmpty(order.OrderProductDetails);
-        }
-
-        [Fact]
         public void GetSingleFullOrderBy_ReturnsNull_WhenIdDoesNotExist()
         {
             using var context = new ApplicationDbContext(_dbContextOptions);
@@ -254,25 +213,6 @@ namespace xUnitTest
         }
 
         [Fact]
-        public void GetSingleFullOrderBy_ReturnsFullOrder_WhenEmailExists()
-        {
-            using var context = new ApplicationDbContext(_dbContextOptions);
-            
-            // Arrange
-            var service = new OrderService(context);
-            string existingEmail = "seeded@email.com";
-
-            // Act
-            var order = service.GetSingleFullOrderBy(existingEmail);
-
-            // Assert
-            Assert.NotNull(order);
-            Assert.Equal(existingEmail, order.Email);
-            Assert.NotNull(order.OrderProductDetails);
-            Assert.True(order.OrderProductDetails.Count > 0);
-        }
-
-        [Fact]
         public void GetSingleFullOrderBy_ReturnsNull_WhenEmailDoesNotExist()
         {
             using var context = new ApplicationDbContext(_dbContextOptions);
@@ -286,99 +226,6 @@ namespace xUnitTest
 
             // Assert
             Assert.Null(order);
-        }
-
-        [Fact]
-        public void UpdateVinylBy_UpdatesCorrectly_WhenOrderIdExists()
-        {
-            using var context = new ApplicationDbContext(_dbContextOptions);
-            
-            // Arrange
-            var service = new OrderService(context);
-            int existingOrderId = 1;
-
-            var newOrder = new Order
-            {
-                Email = "updated@email.com",
-                BuyDate = DateTime.Now
-            };
-
-            // Act
-            service.UpdateVinylBy(existingOrderId, newOrder);
-
-            // Assert
-            var updatedOrder = context.Orders.Find(existingOrderId);
-            Assert.Equal("updated@email.com", updatedOrder?.Email);
-        }
-
-        [Fact]
-        public void UpdateVinylBy_DoesNothing_WhenOrderIdDoesNotExist()
-        {
-            using var context = new ApplicationDbContext(_dbContextOptions);
-            
-            // Arrange
-            var service = new OrderService(context);
-            int nonExistingOrderId = 999;
-
-            var newOrder = new Order
-            {
-                Email = "updated@email.com",
-                BuyDate = DateTime.Now
-            };
-
-            // Act
-            service.UpdateVinylBy(nonExistingOrderId, newOrder);
-
-            // Assert
-            var order = context.Orders.Find(nonExistingOrderId);
-            Assert.Null(order);
-        }
-
-        [Fact]
-        public void UpdateOrderBy_UpdatesCorrectly_WhenEmailExists()
-        {
-            using var context = new ApplicationDbContext(_dbContextOptions);
-            
-            // Arrange
-            var service = new OrderService(context);
-            string existingEmail = "seeded@email.com";
-            var newOrder = new Order
-            {
-                Email = "new@email.com",
-                BuyDate = DateTime.Now
-            };
-            Assert.NotNull(context.Orders.FirstOrDefault(o => o.Email == existingEmail));
-
-
-            // Act
-            service.UpdateOrderBy(existingEmail, newOrder);
-
-            // Assert
-            var updatedOrder = context.Orders.Where(o => o.Email == newOrder.Email).FirstOrDefault();
-            Assert.NotNull(updatedOrder);
-            Assert.Equal(newOrder.BuyDate, updatedOrder.BuyDate);
-        }
-        
-        [Fact]
-        public void UpdateOrderBy_DoesNothing_WhenEmailDoesNotExist()
-        {
-            using var context = new ApplicationDbContext(_dbContextOptions);
-            
-            // Arrange
-            var service = new OrderService(context);
-            string nonExistingEmail = "nonexisting@email.com";
-            var newOrder = new Order
-            {
-                Email = "new@email.com",
-                BuyDate = DateTime.Now
-            };
-            int initialCount = context.Orders.Count();
-
-            // Act
-            service.UpdateOrderBy(nonExistingEmail, newOrder);
-
-            // Assert
-            Assert.Equal(initialCount, context.Orders.Count());
         }
         
         [Fact]
@@ -398,23 +245,6 @@ namespace xUnitTest
         }
 
         [Fact]
-        public void GetAllOrders_ReturnsEmptyList_WhenNoOrders()
-        {
-            using var context = new ApplicationDbContext(_dbContextOptions);
-            
-            // Arrange
-            var service = new OrderService(context);
-            context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
-
-            // Act
-            var orders = service.GetAllOrders();
-
-            // Assert
-            Assert.Empty(orders);
-        }
-
-        [Fact]
         public void GetAllFullOrders_ReturnsAllOrdersWithDetails()
         {
             using var context = new ApplicationDbContext(_dbContextOptions);
@@ -429,23 +259,6 @@ namespace xUnitTest
             // Assert
             Assert.Equal(initialCount, orders.Count);
             Assert.All(orders, order => Assert.NotEmpty(order.OrderProductDetails));
-        }
-
-        [Fact]
-        public void GetAllFullOrders_ReturnsEmptyList_WhenNoOrders()
-        {
-            using var context = new ApplicationDbContext(_dbContextOptions);
-            
-            // Arrange
-            var service = new OrderService(context);
-            context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
-
-            // Act
-            var orders = service.GetAllFullOrders().ToList();
-
-            // Assert
-            Assert.Empty(orders);
         }
         
         #endregion
