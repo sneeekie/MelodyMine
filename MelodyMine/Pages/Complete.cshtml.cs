8d6737aec775973338f97e40d516c008b5d43cf5
Adrian Nuppnau <83693885+sneeekie@users.noreply.github.com>
2023-11-09 08:55:39 +0100

    Page for ShoppingCart / Basket (#11)
    * Add 'Back' buttons for better acceisibility
    
    * Implement /ShoppingCart page for checkout
    
    * Thank you note page, for customer
    
    * Use correct routing for menu 'ShoppingCart'
    
    * Adjusted 'ShoppingCart' for menu
    
    * Changed headers
    
    * Added 'Edit' button for Orders
    
    * Carousel image order
    
    * Adjusted 'Back' buttons for /Edit pages

---
+++ MelodyMine/Pages/Complete.cshtml.cs
@@ -0,0 +1,29 @@
+using DataLayer.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MelodyMine.Pages;
+
+public class CompleteModel : PageModel
+{
+    private readonly IOrderService _orderService;
+
+    public CompleteModel(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+
+    public Order Order { get; private set; }
+
+    public void OnGet(int orderId)
+    {
+        Order = _orderService.GetSingleFullOrderBy(orderId);
+
+        if (Order == null)
+        {
+            // Håndter tilfælde, hvor ordren ikke findes
+            // F.eks. Redirect til en fejlside eller vis en fejlbesked
+        }
+        
+    }
+}
