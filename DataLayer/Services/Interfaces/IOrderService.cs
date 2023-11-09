0cde77af49359f4ae0639b18ca65c6587f7422af
@adrian.istrefi.nuppnau <adrian.istrefi.nuppnau@lego.com>
2023-11-09 12:44:43 +0100

    Order page & services

--- DataLayer/Services/Interfaces/IOrderService.cs
+++ DataLayer/Services/Interfaces/IOrderService.cs
@@ -12,8 +12,11 @@ public interface IOrderService
     public Order GetSingleFullOrderBy(int id);
     public Order GetSingleOrderBy(string email);
     public Order GetSingleFullOrderBy(string email);
-    public void UpdateVinylBy(int orderId, Order newOrder);
-    public void UpdateOrderBy(string orderEmail, Order newOrder);
+    public void UpdateOrderById(int orderId, Order newOrder);
+    public void UpdateOrderByEmail(string orderEmail, Order newOrder);
     public IQueryable<Order> GetAllOrders();
     public IQueryable<Order> GetAllFullOrders();
+    public void UpdateAddress(int addressId, Address newAddress);
+    public void UpdateOrderProductDetails(List<OrderProductDetails> newOrderProductDetails);
+    public bool OrderExists(int id);
 }
